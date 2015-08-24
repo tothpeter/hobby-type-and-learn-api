9601bdcd45011e7900af9834a0203c56c9ea085e
Peter Toth <tothpeter08@gmail.com>
2015-08-24 20:45:13 +0200

    Add tests for generate auth token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe User, type: :model do
 
   it { should validate_uniqueness_of(:email) }
   it { should validate_uniqueness_of(:auth_token_for_web)}
+
+  describe "#generate_authentication_token!" do
+    it "generates a token" do
+      allow(Devise).to receive_messages(friendly_token: "auniquetoken123")
+
+      @user.generate_authentication_token!
+      expect(@user.auth_token_for_web).to eql "auniquetoken123"
+    end
+    
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token_for_web: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token_for_web).not_to eql existing_user.auth_token_for_web
+    end
+  end
 end
