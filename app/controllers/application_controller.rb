b662f3a3d93702cb5ee69710fa134c1ae8708d9e
Peter Toth <tothpeter08@gmail.com>
2015-09-04 01:07:33 +0200

    Transform params to use snake case (ember data tends to sending dasherized params)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::API
   include Authenticable
+
+  before_filter :deep_snake_case_params!
+
+  protected
+
+  def deep_snake_case_params!(val = params)
+    case val
+    when Array
+      val.map {|v| deep_snake_case_params! v }
+    when Hash
+      val.keys.each do |k, v = val[k]|
+        val.delete k
+        val[k.underscore] = deep_snake_case_params!(v)
+      end
+      val
+    else
+      val
+    end
+  end
 end
