9cac45d68615ee860394bca0c16c8b8c04a34825
Peter Toth <tothpeter08@gmail.com>
2015-09-23 00:22:26 +0200

    Add import functionality to card model from an uploaded file

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,50 @@
+require 'roo'
+
 class Card < ActiveRecord::Base
   belongs_to :user
   has_many :label_cards, dependent: :destroy
   has_many :labels, through: :label_cards
 
   validates_presence_of :side_a, :side_b
+
+  def self.import file, user
+    spreadsheet = open_spreadsheet(file)
+
+    accessible_attributes = ["side_a", "side_b", "proficiency_level"]
+
+    header = spreadsheet.row 1
+
+    if accessible_attributes.include? header[0]
+      import_with_headers spreadsheet, header, accessible_attributes, user
+    else
+      import_without_headers spreadsheet, user
+    end
+  end
+
+  private
+
+  def self.import_with_headers spreadsheet, header, accessible_attributes, user
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      card = user.cards.build
+      card.attributes = row.to_hash.slice *accessible_attributes
+      card.save!
+    end
+  end
+
+  def self.import_without_headers spreadsheet, user
+    (1..spreadsheet.last_row).each do |i|
+      card = user.cards.build side_a: spreadsheet.row(i)[0], side_b: spreadsheet.row(i)[1], proficiency_level: spreadsheet.row(i)[2]
+      card.save!
+    end
+  end
+
+  def self.open_spreadsheet file
+    case File.extname file.original_filename
+    when ".csv" then Roo::CSV.new file.path
+    when ".xls" then Roo::Spreadsheet.open file.path, extension: :xls
+    when ".xlsx" then Roo::Spreadsheet.open file.path, extension: :xlsx
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
+  include ActionDispatch::TestProcess
+
   before { @card = FactoryGirl.build(:card) }
   subject { @card }
 
@@ -9,4 +11,42 @@ RSpec.describe Card, type: :model do
 
   it { should validate_presence_of :side_a }
   it { should validate_presence_of :side_b }
+
+  describe "#import" do
+    it "imports .xls without any header specified" do
+      file = fixture_file_upload('/files/without_header.xls', 'application/vnd.ms-excel')
+
+      Card.import file, @card.user
+
+      expect(@card.user.cards.count).to eq 2
+      expect(@card.user.cards.first.side_a).to eq "aa"
+    end
+
+    it "imports .xlsx without any header specified" do
+      file = fixture_file_upload('/files/without_header.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      Card.import file, @card.user
+
+      expect(@card.user.cards.count).to eq 2
+      expect(@card.user.cards.first.side_a).to eq "aa"
+    end
+
+    it "imports .xls with header specified" do
+      file = fixture_file_upload('/files/with_header.xls', 'application/vnd.ms-excel')
+
+      Card.import file, @card.user
+
+      expect(@card.user.cards.count).to eq 2
+      expect(@card.user.cards.first.side_a).to eq "aa"
+    end
+
+    it "imports .xlsx with header specified" do
+      file = fixture_file_upload('/files/with_header.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      Card.import file, @card.user
+
+      expect(@card.user.cards.count).to eq 2
+      expect(@card.user.cards.first.side_a).to eq "aa"
+    end
+  end
 end
