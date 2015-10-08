b23f9262110fe2f7bd9cb64ab902ba88d34ab369
Peter Toth <tothpeter08@gmail.com>
2015-10-08 17:44:52 +0200

    Fix notify browser service if the socket file does not exists

--- app/services/notify_browser_service.rb
+++ app/services/notify_browser_service.rb
@@ -1,15 +1,17 @@
 class NotifyBrowserService
   def self.notify event_type, event_data
-    client = UNIXSocket.open("/tmp/websockets_unix.sock")
+    if File.exist? "/tmp/websockets_unix.sock"
+      client = UNIXSocket.open("/tmp/websockets_unix.sock")
 
-    message = {
-      type: "event",
-      event: event_data
-    }
+      message = {
+        type: "event",
+        event: event_data
+      }
 
-    message[:event][:type] = event_type
+      message[:event][:type] = event_type
 
-    client.print JSON.generate(message)
-    client.close
+      client.print JSON.generate(message)
+      client.close
+    end
   end
 end
